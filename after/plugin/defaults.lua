762f75469dc1ee7f883bccf3dd005ab6c91136f5
nttan <nttan@netcompany.com>
2023-06-04 19:13:42 +0700

    update

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,4 +1,6 @@
 vim.opt.relativenumber = true
 
 vim.keymap.set({ 'i' }, 'jk', '<Esc>', { silent = true })
+vim.api.nvim_set_keymap('t', 'jk', [[<C-\><C-n>]], { noremap = true })
+vim.api.nvim_set_keymap('t', '<C-d>', [[<C-\><C-d>]], { noremap = true })
 

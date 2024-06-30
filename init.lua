929c35b139b4337cf9e26e5269d1f2b7a18a7c48
thanhtan541 <thanhtan004100@gmail.com>
2024-06-30 15:45:32 +0700

    feat: add autoreload when file is changed

--- init.lua
+++ init.lua
@@ -41,6 +41,11 @@ P.S. You can delete this when you're done too. It's your config now :)
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
